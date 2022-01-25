e4fa62b90064cd1ed85982ee23b607f8390fe294
fang.liu <i@liufang.org.cn>
2022-01-25 11:42:03 +0800

    add file

---
+++ soho-admin/soho-admin-biz/src/main/java/work/soho/admin/service/AdminRoleService.java
@@ -0,0 +1,24 @@
+package work.soho.admin.service;
+
+import org.springframework.stereotype.Service;
+import org.springframework.beans.factory.annotation.Autowired;
+import work.soho.api.admin.po.AdminRole;
+import work.soho.admin.mapper.AdminRoleMapper;
+
+@Service
+public class AdminRoleService {
+   @Autowired
+   private AdminRoleMapper adminroleMapper;
+
+   public AdminRole getById(Integer id) {
+       return adminroleMapper.getById(id);
+   }
+
+   public int insert(AdminRole adminrole) {
+       return adminroleMapper.insert(adminrole);
+   }
+
+   public int update(AdminRole adminrole) {
+       return adminroleMapper.update(adminrole);
+   }
+}
