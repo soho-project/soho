be8caa453bd512ebf2de8b171882d644dc7d94d1
fang <i@liufang.org.cn>
2022-04-04 02:16:19 +0800

    用户绑定角色

---
+++ soho-admin/soho-admin-biz/src/main/java/work/soho/admin/service/impl/AdminRoleResourceServiceImpl.java
@@ -0,0 +1,22 @@
+package work.soho.admin.service.impl;
+
+import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import work.soho.admin.domain.AdminRoleResource;
+import work.soho.admin.service.AdminRoleResourceService;
+import work.soho.admin.mapper.AdminRoleResourceMapper;
+import org.springframework.stereotype.Service;
+
+/**
+* @author i
+* @description 针对表【admin_role_resource】的数据库操作Service实现
+* @createDate 2022-04-01 00:23:51
+*/
+@Service
+public class AdminRoleResourceServiceImpl extends ServiceImpl<AdminRoleResourceMapper, AdminRoleResource>
+    implements AdminRoleResourceService{
+
+}
+
+
+
+
