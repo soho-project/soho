c4202cd4e5a0ce507c9754d1e07ad8c817c0faa7
fang <i@liufang.org.cn>
2022-04-15 23:45:19 +0800

    添加锁支持

---
+++ soho-common/soho-common-data/src/main/java/work/soho/common/data/lock/config/RedissonConfig.java
@@ -0,0 +1,27 @@
+package work.soho.common.data.lock.config;
+
+import org.redisson.Redisson;
+import org.redisson.api.RedissonClient;
+import org.redisson.config.Config;
+import org.redisson.config.TransportMode;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * https://github.com/redisson/redisson/wiki/2.-%E9%85%8D%E7%BD%AE%E6%96%B9%E6%B3%95
+ */
+@Configuration
+public class RedissonConfig {
+    @Value("${redisson.nodeAddress}")
+    private String redisHost;
+
+    @Bean
+    public RedissonClient getRedissonClient() {
+        Config config = new Config();
+        config.setTransportMode(TransportMode.NIO);
+        config.useSingleServer().setAddress(redisHost).setDatabase(0);
+        RedissonClient redisson = Redisson.create(config);
+        return redisson;
+    }
+}
