fb32f890e285c6e0ec005b3412e02cfb2ee5c08f
fang <i@liufang.org.cn>
2022-04-17 23:48:33 +0800

    整理代码 &ä¿ 修复类引入错误的问题

--- soho-common/soho-common-data/src/main/java/work/soho/common/data/excel/view/DefaultExcelView.java
+++ soho-common/soho-common-data/src/main/java/work/soho/common/data/excel/view/DefaultExcelView.java
@@ -1,12 +1,10 @@
 package work.soho.common.data.excel.view;
 
 import org.apache.poi.hssf.usermodel.HSSFDataFormat;
-import org.apache.poi.ss.usermodel.CellStyle;
-import org.apache.poi.ss.usermodel.Row;
-import org.apache.poi.ss.usermodel.Sheet;
-import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.ss.usermodel.*;
 import org.springframework.web.servlet.view.document.AbstractXlsView;
 import work.soho.common.data.excel.annotation.ExcelColumn;
+import work.soho.common.data.excel.model.ExcelModel;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -14,9 +12,7 @@ import java.io.OutputStream;
 import java.lang.reflect.Field;
 import java.lang.reflect.ParameterizedType;
 import java.lang.reflect.Type;
-import java.util.Arrays;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 import java.util.concurrent.atomic.AtomicInteger;
 
 public class DefaultExcelView extends AbstractXlsView {
@@ -29,38 +25,69 @@ public class DefaultExcelView extends AbstractXlsView {
 
     @Override
     protected void buildExcelDocument(Map<String, Object> model, Workbook workbook, HttpServletRequest request, HttpServletResponse response) throws Exception {
-//        Class<Object> objectClass = (Class)model.get("class");
-        List<Object> list = (List<Object>) model.get("list");
-        Class<?> objectClass = list.get(0).getClass();
-//                System.out.println(list.get(0).getClass().getTypeName());
-
-        String fileName = "用户列表excel.xls";
-        response.setCharacterEncoding("UTF-8");
-        response.setContentType("application/ms-excel");
-        response.setHeader("Content-Disposition", "inline; filename="+new String(fileName.getBytes(),"iso8859-1"));
-        OutputStream outputStream = response.getOutputStream();
-        // 产生Excel表头
-        int rowNumber = 0;
-        Sheet sheet = workbook.createSheet("基本信息");
-        Row header = sheet.createRow(rowNumber++);
-        //输出excel Title
-        AtomicInteger hi = new AtomicInteger();
-        Arrays.stream(objectClass.getDeclaredFields()).forEach(field -> {
-            ExcelColumn excelColumn = field.getAnnotation(ExcelColumn.class);
-            header.createCell(hi.getAndIncrement()).setCellValue(excelColumn.value());
+        ExcelModel excelModel = new ExcelModel();
+        model.forEach((key, value)->{
+            excelModel.put(key, value);
         });
 
-        for (int i = 0; i < list.size(); i++) {
-            Row row = sheet.createRow(rowNumber++);
-            for (int i1 = 0; i1 < objectClass.getDeclaredFields().length; i1++) {
-                Field field = objectClass.getDeclaredFields()[i1];
-                field.setAccessible(true);
-                row.createCell(i1).setCellValue(String.valueOf(field.get(list.get(i))));
-            }
-        }
+        response.setCharacterEncoding("UTF-8");
+        response.setContentType("application/ms-excel");
+        response.setHeader("Content-Disposition", "inline; filename="+new String(excelModel.getFileName().getBytes(),"iso8859-1"));
+        OutputStream outputStream = response.getOutputStream();
 
-        CellStyle cellStyle = workbook.createCellStyle();
-        cellStyle.setDataFormat(HSSFDataFormat.getBuiltinFormat("mm/dd/yyyy"));
+        excelModel.getSheetList().forEach(sheetOriginData -> {
+            HashMap<String, Object> sheetData = (HashMap<String, Object>)sheetOriginData;
+            String sheetName = (String) sheetData.get(ExcelModel.SHEET_NAME);
+            Class<?> modelClass = (Class<?>) sheetData.get(ExcelModel.SHEET_CLASS);
+            List<?> data = (List<?>) sheetData.get(ExcelModel.SHEET_DATA);
+            // 产生Excel表头
+            int rowNumber = 0;
+            Sheet sheet = workbook.createSheet(sheetName);
+            Row header = sheet.createRow(rowNumber++);
+            //输出excel Title
+            AtomicInteger hi = new AtomicInteger();
+            Arrays.stream(modelClass.getDeclaredFields()).forEach(field -> {
+                ExcelColumn excelColumn = field.getAnnotation(ExcelColumn.class);
+                if(excelColumn == null) {
+                    return;
+                }
+                header.createCell(hi.getAndIncrement()).setCellValue(excelColumn.value());
+            });
+
+            for (int i = 0; i < data.size(); i++) {
+                Row row = sheet.createRow(rowNumber++);
+                int celNum = 0;
+                for (int i1 = 0; i1 < modelClass.getDeclaredFields().length; i1++) {
+                    Field field = modelClass.getDeclaredFields()[i1];
+                    ExcelColumn excelColumn = field.getAnnotation(ExcelColumn.class);
+                    if(excelColumn == null) {
+                        continue;
+                    }
+                    field.setAccessible(true);
+                    try {
+                        Object v = field.get(data.get(i));
+                        if(v instanceof Date) {
+                            CellStyle cellStyle = workbook.createCellStyle();
+                            CreationHelper createHelper = workbook.getCreationHelper();
+                            short dateFormat = createHelper.createDataFormat().getFormat(excelColumn.dateFormat());
+                            cellStyle.setDataFormat(dateFormat);
+                            row.createCell(celNum).setCellStyle(cellStyle);
+                            row.getCell(celNum).setCellValue((Date) v);
+                        } else if(v instanceof Integer) {
+                            row.createCell(celNum).setCellValue((Integer)v);
+                        }else if(v instanceof Long) {
+                            row.createCell(celNum).setCellValue((Long)v);
+                        } else {
+                            row.createCell(celNum).setCellValue(String.valueOf(v));
+                        }
+                        celNum++;
+                    } catch (IllegalAccessException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+
+        });
 
         workbook.write(outputStream);
         outputStream.flush();
