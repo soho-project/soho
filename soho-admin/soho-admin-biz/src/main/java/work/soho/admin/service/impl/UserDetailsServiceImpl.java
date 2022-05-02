41bbcf8a7318871ee961ec76cccf58efad411729
fang <i@liufang.org.cn>
2022-05-02 18:22:53 +0800

    抽离权限控制接口

--- soho-admin/soho-admin-biz/src/main/java/work/soho/admin/service/impl/UserDetailsServiceImpl.java
+++ soho-admin/soho-admin-biz/src/main/java/work/soho/admin/service/impl/UserDetailsServiceImpl.java
@@ -1,24 +1,20 @@
 package work.soho.admin.service.impl;
 
-import lombok.Data;
 import lombok.RequiredArgsConstructor;
-import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
+import work.soho.admin.common.security.service.SohoSecurityUserDetailsService;
 import work.soho.admin.domain.AdminUser;
 import work.soho.admin.service.AdminUserService;
-
-import java.util.Collection;
 
 /**
  * 用户鉴权相关实现
  */
 @Service
 @RequiredArgsConstructor
-public class UserDetailsServiceImpl implements UserDetailsService {
+public class UserDetailsServiceImpl implements SohoSecurityUserDetailsService {
     private final AdminUserService adminUserService;
 
     @Override
@@ -26,7 +22,6 @@ public class UserDetailsServiceImpl implements UserDetailsService {
         AdminUser adminUser = adminUserService.getByLoginName(username);
         if(adminUser != null) {
             UserDetailsImpl userDetails = new UserDetailsImpl();
-            assert adminUser != null;
             userDetails.setUsername(String.valueOf(adminUser.getPhone()));
             userDetails.setPassword(adminUser.getPassword());
             userDetails.setId(adminUser.getId());
@@ -41,55 +36,8 @@ public class UserDetailsServiceImpl implements UserDetailsService {
      * @return
      */
     public UserDetails getLoginUserDetails() {
-        UserDetailsServiceImpl.UserDetailsImpl userDetails = (UserDetailsServiceImpl.UserDetailsImpl) SecurityContextHolder.getContext()
+        return (UserDetailsServiceImpl.UserDetailsImpl) SecurityContextHolder.getContext()
                 .getAuthentication()
                 .getPrincipal();
-        return userDetails;
-    }
-
-    @Data
-    public static class UserDetailsImpl implements UserDetails {
-        private Long id;
-        private String password;
-        private String username;
-
-        @Override
-        public Collection<? extends GrantedAuthority> getAuthorities() {
-            return null;
-        }
-
-        @Override
-        public String getPassword() {
-            return password;
-        }
-
-        @Override
-        public String getUsername() {
-            return username;
-        }
-
-        public Long getId() {
-            return id;
-        }
-
-        @Override
-        public boolean isAccountNonExpired() {
-            return true;
-        }
-
-        @Override
-        public boolean isAccountNonLocked() {
-            return true;
-        }
-
-        @Override
-        public boolean isCredentialsNonExpired() {
-            return true;
-        }
-
-        @Override
-        public boolean isEnabled() {
-            return true;
-        }
     }
 }
