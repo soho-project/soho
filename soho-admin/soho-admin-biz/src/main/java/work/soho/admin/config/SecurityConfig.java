b4b5d8e4d3fa9283d9056efc1872fdca4c9b3d26
fang <i@liufang.org.cn>
2022-05-02 17:09:24 +0800

    fixed empty list && add redis docker file

--- soho-admin/soho-admin-biz/src/main/java/work/soho/admin/config/SecurityConfig.java
+++ soho-admin/soho-admin-biz/src/main/java/work/soho/admin/config/SecurityConfig.java
@@ -94,7 +94,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter
                 // 过滤请求
                 .authorizeRequests()
                 // 对于登录login 验证码captchaImage 和其他放行的目录 允许匿名访问"/citylife/front/**"
-                .antMatchers("/login", "/captchaImage").permitAll()
+                .antMatchers("/login", "/captcha").permitAll()
 //                .antMatchers("/user").hasAuthority("admin")
                 .antMatchers(
                         HttpMethod.GET,
