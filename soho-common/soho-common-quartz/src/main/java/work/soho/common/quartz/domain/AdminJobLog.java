d5ac524eee1824d6828ed84abb4edb45bcf89c48
fang <i@liufang.org.cn>
2022-08-02 03:34:15 +0800

    提交计划任务相关业务实现

---
+++ soho-common/soho-common-quartz/src/main/java/work/soho/common/quartz/domain/AdminJobLog.java
@@ -0,0 +1,62 @@
+package work.soho.common.quartz.domain;
+
+import com.baomidou.mybatisplus.annotation.IdType;
+import com.baomidou.mybatisplus.annotation.TableField;
+import com.baomidou.mybatisplus.annotation.TableId;
+import com.baomidou.mybatisplus.annotation.TableName;
+import java.io.Serializable;
+import java.time.LocalDateTime;
+import lombok.Data;
+
+/**
+ * 计划任务执行日志
+ * @TableName admin_job_log
+ */
+@TableName(value ="admin_job_log")
+@Data
+public class AdminJobLog implements Serializable {
+    /**
+     * 
+     */
+    @TableField(value = "id")
+    private Integer id;
+
+    /**
+     * 任务ID
+     */
+    @TableField(value = "job_id")
+    private Integer jobId;
+
+    /**
+     * 任务创建时间
+     */
+    @TableField(value = "start_time")
+    private LocalDateTime startTime;
+
+    /**
+     * 任务执行状态; 1 执行中  2 执行完成  3 任务取消
+     */
+    @TableField(value = "status")
+    private Integer status;
+
+    /**
+     * 任务执行返回结果
+     */
+    @TableField(value = "result")
+    private String result;
+
+    /**
+     * 任务结束时间
+     */
+    @TableField(value = "end_time")
+    private LocalDateTime endTime;
+
+    /**
+     * 手动执行时候的用户ID
+     */
+    @TableField(value = "admin_id")
+    private Integer adminId;
+
+    @TableField(exist = false)
+    private static final long serialVersionUID = 1L;
+}
---
+++ soho-common/soho-common-quartz/src/main/java/work/soho/common/quartz/service/AdminJobService.java
@@ -0,0 +1,15 @@
+package work.soho.common.quartz.service;
+
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
+import work.soho.common.quartz.domain.AdminJob;
+import com.baomidou.mybatisplus.extension.service.IService;
+
+import java.util.List;
+
+/**
+* @author i
+* @description 针对表【admin_job】的数据库操作Service
+* @createDate 2022-07-26 03:42:42
+*/
+public interface AdminJobService extends IService<AdminJob> {
+}
