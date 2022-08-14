3e7fed2367dc3b02bff219b3a55be58a4b4365d3
fang <i@liufang.org.cn>
2022-08-15 00:59:05 +0800

    调整目录结构

--- Dockerfile
+++ Dockerfile
@@ -2,8 +2,8 @@ FROM openjdk:11
 LABEL maintainer="i@liufang.org.cn" \
 	  version="v1.0" \
 	  description="Java11; 打包soho-admin服务"
-COPY ./soho-admin/soho-admin-biz/target/soho-admin-biz-1.0-SNAPSHOT.jar /root/
+COPY ./soho-admin-web/target/soho-admin-web-1.0-SNAPSHOT.jar /root/
 WORKDIR /root/
 ARG CONFIG_PROFILE="dev"
 ENV SPRING_CLOUD_CONFIG_PROFILE="-Dspring.profiles.active=${CONFIG_PROFILE}"
-CMD java -jar ${SPRING_CLOUD_CONFIG_PROFILE} /root/soho-admin-biz-1.0-SNAPSHOT.jar
+CMD java -jar ${SPRING_CLOUD_CONFIG_PROFILE} /root/soho-admin-web-1.0-SNAPSHOT.jar
---
+++ soho-admin-web/src/main/java/work/soho/web/AdminApplication.java
@@ -0,0 +1,17 @@
+package work.soho.web;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication(scanBasePackages = {"work.soho"})
+@MapperScan({"work.soho.admin.mapper"
+		, "work.soho.approvalprocess.mapper"
+		, "work.soho.common.quartz.mapper"
+		, "com.baomidou.mybatisplus.samples.quickstart.mapper"})
+public class AdminApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(AdminApplication.class);
+	}
+
+}
