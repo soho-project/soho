8026e0ad6f72e1c1d43c3976e0c4d48416fe9159
fang.liu <i@liufang.org.cn>
2022-11-24 04:00:59 +0800

    添加自动化tree支持

---
+++ soho-admin/soho-admin-api/src/main/java/work/soho/api/admin/vo/TreeNodeVo.java
@@ -0,0 +1,26 @@
+package work.soho.api.admin.vo;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.Data;
+import lombok.RequiredArgsConstructor;
+
+import java.util.List;
+
+@Data
+public class TreeNodeVo<T,F,C,X> {
+    private T key;
+    private F value;
+    private C  parentId;
+    private X title;
+
+
+    @JsonInclude(JsonInclude.Include.NON_EMPTY)
+    List<TreeNodeVo> children;
+
+    public TreeNodeVo(T id, F id1, C parentId, X title) {
+        this.key = id;
+        this.value = id1;
+        this.parentId = parentId;
+        this.title = title;
+    }
+}
