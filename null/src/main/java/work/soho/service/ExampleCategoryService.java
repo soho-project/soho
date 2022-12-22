10c975e7b9d356dd034844800cfde962208f90a9
fang.liu <i@liufang.org.cn>
2022-12-22 21:53:14 +0800

    修复测试执行模板选择表的问题

---
+++ null/src/main/java/work/soho/service/ExampleCategoryService.java
@@ -0,0 +1,8 @@
+package work.soho.service;
+
+import work.soho.domain.ExampleCategory;
+import com.baomidou.mybatisplus.extension.service.IService;
+
+public interface ExampleCategoryService extends IService<ExampleCategory> {
+
+}
