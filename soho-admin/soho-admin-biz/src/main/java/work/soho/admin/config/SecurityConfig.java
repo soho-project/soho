d6a29eed97d1336ce962e6de84106d08f3431e8d
fang.liu <i@liufang.org.cn>
2023-06-26 23:19:50 +0800

    修正鉴权依赖错误

--- soho-admin/soho-admin-biz/src/main/java/work/soho/admin/config/SecurityConfig.java
+++ soho-admin/soho-admin-biz/src/main/java/work/soho/admin/config/SecurityConfig.java
@@ -1,12 +1,16 @@
 package work.soho.admin.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -15,11 +19,6 @@ import org.springframework.security.web.authentication.UsernamePasswordAuthentic
 import work.soho.admin.filter.AuthenticationEntryPointImpl;
 import work.soho.admin.filter.JwtAuthFilter;
 
-/**
- * spring security配置
- *
- * @author ruoyi
- */
 @EnableGlobalMethodSecurity(prePostEnabled = true, securedEnabled = true)
 public class SecurityConfig extends WebSecurityConfigurerAdapter
 {
@@ -36,22 +35,10 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter
     private AuthenticationEntryPointImpl unauthorizedHandler;
 
     /**
-     * 退出处理类
-     */
-//    @Autowired
-//    private LogoutSuccessHandlerImpl logoutSuccessHandler;
-
-    /**
      * token认证过滤器
      */
     @Autowired
     private JwtAuthFilter authenticationTokenFilter;
-
-    /**
-     * 跨域过滤器
-     */
-//    @Autowired
-//    private CorsFilter corsFilter;
 
     /**
      * 解决 无法直接注入 AuthenticationManager
@@ -66,21 +53,6 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter
         return super.authenticationManagerBean();
     }
 
-    /**
-     * anyRequest          |   匹配所有请求路径
-     * access              |   SpringEl表达式结果为true时可以访问
-     * anonymous           |   匿名可以访问
-     * denyAll             |   用户不能访问
-     * fullyAuthenticated  |   用户完全认证可以访问（非remember-me下自动登录）
-     * hasAnyAuthority     |   如果有参数，参数表示权限，则其中任何一个权限可以访问
-     * hasAnyRole          |   如果有参数，参数表示角色，则其中任何一个角色可以访问
-     * hasAuthority        |   如果有参数，参数表示权限，则其权限可以访问
-     * hasIpAddress        |   如果有参数，参数表示IP地址，如果用户IP和参数匹配，则可以访问
-     * hasRole             |   如果有参数，参数表示角色，则其角色可以访问
-     * permitAll           |   用户可以任意访问
-     * rememberMe          |   允许通过remember-me登录的用户访问
-     * authenticated       |   用户登录后可访问
-     */
     @Override
     protected void configure(HttpSecurity httpSecurity) throws Exception
     {
