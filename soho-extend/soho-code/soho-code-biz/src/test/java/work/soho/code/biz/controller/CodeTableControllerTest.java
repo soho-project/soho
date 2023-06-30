753da78aaf6d0815785a5913f89069a72a8d3167
fang.liu <i@liufang.org.cn>
2023-07-01 00:54:59 +0800

    修复导入数据库表无法导入主键的问题

---
+++ soho-extend/soho-code/soho-code-biz/src/test/java/work/soho/code/biz/controller/CodeTableControllerTest.java
@@ -0,0 +1,69 @@
+package work.soho.code.biz.controller;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+import work.soho.code.biz.CodeApplication;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@ContextConfiguration
+@WebAppConfiguration("src/main/resources")
+@SpringBootTest(classes = CodeApplication.class)
+class CodeTableControllerTest {
+    private MockMvc mockMvc;
+
+    @Autowired
+    private WebApplicationContext webApplicationContext;
+
+    @BeforeEach
+    public void setup() {
+        this.mockMvc = MockMvcBuilders.webAppContextSetup(this.webApplicationContext).build();
+    }
+
+    @Test
+    void sql() {
+    }
+
+    @Test
+    void exec() {
+    }
+
+    @Test
+    void getCode() {
+    }
+
+    @Test
+    void saveCodes() {
+    }
+
+    @Test
+    void createCodes() {
+    }
+
+    @Test
+    void zipCodes() {
+    }
+
+    @Test
+    void getTables() {
+    }
+
+    @Test
+    void loadTable() throws Exception {
+        System.out.println("load table");
+        mockMvc.perform(get("/admin/codeTable/saveTables").param("tableNames", "pay_info").contentType("application/json"))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andReturn();
+    }
+}
