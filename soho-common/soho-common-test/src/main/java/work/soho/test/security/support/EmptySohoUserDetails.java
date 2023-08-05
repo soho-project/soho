eefd76295f3ff939f04894ee8d16f1435ddf0c08
fang.liu <i@liufang.org.cn>
2023-08-05 23:20:07 +0800

    修正测试模块

---
+++ soho-common/soho-common-test/src/main/java/work/soho/test/security/support/EmptySohoUserDetails.java
@@ -0,0 +1,18 @@
+package work.soho.test.security.support;
+
+import org.springframework.stereotype.Service;
+import work.soho.admin.common.security.service.SohoUserDetailsService;
+import work.soho.admin.common.security.userdetails.SohoUserDetails;
+
+@Service
+public class EmptySohoUserDetails implements SohoUserDetailsService {
+    @Override
+    public SohoUserDetails loadUserByUsername(String username) {
+        return null;
+    }
+
+    @Override
+    public String getUserRoleName() {
+        return null;
+    }
+}
