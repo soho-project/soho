619f643edacdb46ec51be71b0035891c915abdfe
fang.liu <i@liufang.org.cn>
2023-08-25 23:49:47 +0800

    添加好友申请接口&添加好友查询接口

---
+++ soho-extend/soho-chat/soho-chat-biz/src/main/java/work/soho/chat/biz/vo/UserVO.java
@@ -0,0 +1,11 @@
+package work.soho.chat.biz.vo;
+
+import lombok.Data;
+
+@Data
+public class UserVO {
+    private Long id;
+    private String username;
+    private String nickname;
+    private Boolean isFriend = false;
+}
