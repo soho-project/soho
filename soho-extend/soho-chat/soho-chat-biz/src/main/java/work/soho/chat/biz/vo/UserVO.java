3ef07ba6010f124f75581d06f4b501f38a599bfd
fang.liu <i@liufang.org.cn>
2023-11-06 15:42:57 +0800

    添加好友查找用户接口

--- soho-extend/soho-chat/soho-chat-biz/src/main/java/work/soho/chat/biz/vo/UserVO.java
+++ soho-extend/soho-chat/soho-chat-biz/src/main/java/work/soho/chat/biz/vo/UserVO.java
@@ -5,6 +5,7 @@ import lombok.Data;
 @Data
 public class UserVO {
     private Long id;
+    private String avatar;
     private String username;
     private String nickname;
     private Boolean isFriend = false;
