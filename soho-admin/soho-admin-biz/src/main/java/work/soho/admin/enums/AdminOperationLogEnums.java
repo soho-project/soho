45348f03ea442342c95e5e60be8bd24801a62c76
fang.liu <i@liufang.org.cn>
2024-05-02 20:40:22 +0800

    新增系统平台操作日志

---
+++ soho-admin/soho-admin-biz/src/main/java/work/soho/admin/enums/AdminOperationLogEnums.java
@@ -0,0 +1,15 @@
+package work.soho.admin.enums;
+
+public class AdminOperationLogEnums {
+
+//    @RequiredArgsConstructor
+//    @Getter
+//    public enum Method {
+//        DELETE(DELETE,"DELETE"),
+//        POST(POST,"POST"),
+//        GET(GET,"GET"),
+//        PUT(PUT,"PUT");
+//        private final int id;
+//        private final String name;
+//    }
+}
---
+++ soho-admin/soho-admin-biz/src/main/java/work/soho/admin/mapper/AdminOperationLogMapper.java
@@ -0,0 +1,8 @@
+package work.soho.admin.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import work.soho.admin.domain.AdminOperationLog;
+
+public interface AdminOperationLogMapper extends BaseMapper<AdminOperationLog> {
+
+}
