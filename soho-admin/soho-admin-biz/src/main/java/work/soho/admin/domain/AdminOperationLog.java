814742dad8a2f6c99844e7fc35c0566497fd6a15
fang.liu <i@liufang.org.cn>
2024-08-10 01:16:52 +0800

    系统模块相关api文档整理

--- soho-admin/soho-admin-biz/src/main/java/work/soho/admin/domain/AdminOperationLog.java
+++ soho-admin/soho-admin-biz/src/main/java/work/soho/admin/domain/AdminOperationLog.java
@@ -5,6 +5,7 @@ import com.baomidou.mybatisplus.annotation.TableField;
 import com.baomidou.mybatisplus.annotation.TableId;
 import com.baomidou.mybatisplus.annotation.TableName;
 import com.fasterxml.jackson.annotation.JsonFormat;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
 
 import java.io.Serializable;
@@ -16,48 +17,56 @@ public class AdminOperationLog implements Serializable {
     /**
     * ID
     */
+    @ApiModelProperty(value = "ID")
     @TableId(type = IdType.AUTO)
     private Integer id;
 
     /**
     * 管理员;;frontType:select,foreign:admin_user.id~username,isFilter:true
     */
+    @ApiModelProperty(value = "管理员;;frontType:select,foreign:admin_user.id~username,isFilter:true")
     @TableField(value = "admin_user_id")
     private Long adminUserId;
 
     /**
     * 请求方法;GET:GET,POST:POST,DELETE:DELETE,PUT:PUT;frontType:select,
     */
+    @ApiModelProperty(value = "请求方法;GET:GET,POST:POST,DELETE:DELETE,PUT:PUT;frontType:select,")
     @TableField(value = "method")
     private String method;
 
     /**
     * 请求路径
     */
+    @ApiModelProperty(value = "请求路径")
     @TableField(value = "path")
     private String path;
 
     /**
     * 请求参数
     */
+    @ApiModelProperty(value = "请求参数")
     @TableField(value = "params")
     private String params;
 
     /**
     * 操作内容;;frontType:text,isFilter:true
     */
+    @ApiModelProperty(value = "操作内容;;")
     @TableField(value = "content")
     private String content;
 
     /**
     * 返回数据
     */
+    @ApiModelProperty(value = "返回数据")
     @TableField(value = "response")
     private String response;
 
     /**
     * 更新时间
     */
+    @ApiModelProperty(value = "更新时间")
     @TableField(value = "updated_time")
     @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
     private LocalDateTime updatedTime;
@@ -65,8 +74,8 @@ public class AdminOperationLog implements Serializable {
     /**
     * 创建时间
     */
+    @ApiModelProperty(value = "创建时间")
     @TableField(value = "created_time")
     @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
     private LocalDateTime createdTime;
-
 }
--- soho-admin/soho-admin-biz/src/main/java/work/soho/admin/domain/AdminRelease.java
+++ soho-admin/soho-admin-biz/src/main/java/work/soho/admin/domain/AdminRelease.java
@@ -5,34 +5,40 @@ import com.baomidou.mybatisplus.annotation.TableField;
 import com.baomidou.mybatisplus.annotation.TableId;
 import com.baomidou.mybatisplus.annotation.TableName;
 import com.fasterxml.jackson.annotation.JsonFormat;
-import java.io.Serializable;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
+
+import java.io.Serializable;
 import java.time.LocalDateTime;
 
 @TableName(value ="admin_release")
 @Data
 public class AdminRelease implements Serializable {
     /**
-    * ;;
+    * ID
     */
+    @ApiModelProperty(value = "ID")
     @TableId(value = "id", type = IdType.AUTO)
     private Integer id;
 
     /**
     * 版本号
     */
+    @ApiModelProperty(value = "版本号")
     @TableField(value = "version")
     private String version;
 
     /**
     * 发版描述
     */
+    @ApiModelProperty(value = "发版描述")
     @TableField(value = "notes")
     private String notes;
 
     /**
     * 创建时间
     */
+    @ApiModelProperty(value = "创建时间")
     @TableField(value = "created_time")
     @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
     private LocalDateTime createdTime;
@@ -40,6 +46,7 @@ public class AdminRelease implements Serializable {
     /**
     * 更新时间
     */
+    @ApiModelProperty(value = "更新时间")
     @TableField(value = "updated_time")
     @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
     private LocalDateTime updatedTime;
@@ -47,13 +54,14 @@ public class AdminRelease implements Serializable {
     /**
     * 下载地址
     */
+    @ApiModelProperty(value = "下载地址")
     @TableField(value = "url")
     private String url;
 
     /**
     * 平台类型;1:Windows,2:Linux;frontType:select,
     */
+    @ApiModelProperty(value = "平台类型;1:Windows,2:Linux;frontType:select,option:platform_typeEnum")
     @TableField(value = "platform_type")
     private Integer platformType;
-
 }
