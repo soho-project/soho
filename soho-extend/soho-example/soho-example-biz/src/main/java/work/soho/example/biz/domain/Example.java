168dceaac3616234300ab9e7e3d7dcd24ac19f0e
fang.liu <i@liufang.org.cn>
2024-08-18 01:55:03 +0800

    完善代码自动化文档 && 添加单元测试用例

--- soho-extend/soho-example/soho-example-biz/src/main/java/work/soho/example/biz/domain/Example.java
+++ soho-extend/soho-example/soho-example-biz/src/main/java/work/soho/example/biz/domain/Example.java
@@ -1,15 +1,15 @@
 package work.soho.example.biz.domain;
 
-import com.alibaba.excel.annotation.ExcelProperty;
-import com.alibaba.excel.annotation.format.DateTimeFormat;
+import io.swagger.annotations.ApiModelProperty;
 import com.baomidou.mybatisplus.annotation.IdType;
 import com.baomidou.mybatisplus.annotation.TableField;
 import com.baomidou.mybatisplus.annotation.TableId;
 import com.baomidou.mybatisplus.annotation.TableName;
 import com.fasterxml.jackson.annotation.JsonFormat;
-import lombok.Data;
-
+import com.alibaba.excel.annotation.ExcelProperty;
+import com.alibaba.excel.annotation.format.DateTimeFormat;
 import java.io.Serializable;
+import lombok.Data;
 import java.time.LocalDateTime;
 
 @TableName(value ="example")
@@ -19,6 +19,7 @@ public class Example implements Serializable {
     * ID
     */
     @ExcelProperty("ID")
+    @ApiModelProperty(value = "ID")
     @TableId(value = "id", type = IdType.AUTO)
     private Integer id;
 
@@ -26,6 +27,7 @@ public class Example implements Serializable {
     * 标题
     */
     @ExcelProperty("标题")
+    @ApiModelProperty(value = "标题")
     @TableField(value = "title")
     private String title;
 
@@ -33,6 +35,7 @@ public class Example implements Serializable {
     * 分类ID
     */
     @ExcelProperty("分类ID")
+    @ApiModelProperty(value = "分类ID")
     @TableField(value = "category_id")
     private Integer categoryId;
 
@@ -40,6 +43,7 @@ public class Example implements Serializable {
     * 选项
     */
     @ExcelProperty("选项")
+    @ApiModelProperty(value = "选项")
     @TableField(value = "option_id")
     private String optionId;
 
@@ -47,6 +51,7 @@ public class Example implements Serializable {
     * 富媒体
     */
     @ExcelProperty("富媒体")
+    @ApiModelProperty(value = "富媒体")
     @TableField(value = "content")
     private String content;
 
@@ -54,6 +59,7 @@ public class Example implements Serializable {
     * 更新时间
     */
     @ExcelProperty("更新时间")
+    @ApiModelProperty(value = "更新时间")
     @TableField(value = "updated_time")
     @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
     @DateTimeFormat("yyyy-MM-dd HH:mm:ss")
@@ -63,6 +69,7 @@ public class Example implements Serializable {
     * 创建时间
     */
     @ExcelProperty("创建时间")
+    @ApiModelProperty(value = "创建时间")
     @TableField(value = "created_time")
     @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
     @DateTimeFormat("yyyy-MM-dd HH:mm:ss")
@@ -71,7 +78,8 @@ public class Example implements Serializable {
     /**
     * 审批状态
     */
-    @ExcelProperty("申请状态")
+    @ExcelProperty("审批状态")
+    @ApiModelProperty(value = "审批状态")
     @TableField(value = "apply_status")
     private Integer applyStatus;
 
@@ -79,20 +87,23 @@ public class Example implements Serializable {
     * 状态
     */
     @ExcelProperty("状态")
+    @ApiModelProperty(value = "状态")
     @TableField(value = "status")
     private Integer status;
 
     /**
     * 用户
     */
-    @ExcelProperty("用户ID")
+    @ExcelProperty("用户")
+    @ApiModelProperty(value = "用户")
     @TableField(value = "user_id")
     private Integer userId;
 
     /**
     * 开放用户
     */
-    @ExcelProperty("开放用户ID")
+    @ExcelProperty("开放用户")
+    @ApiModelProperty(value = "开放用户")
     @TableField(value = "open_id")
     private Integer openId;
 
