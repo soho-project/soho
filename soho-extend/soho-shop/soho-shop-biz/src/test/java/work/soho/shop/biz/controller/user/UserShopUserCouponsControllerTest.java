c93233f4567fc30dc0bd5de2d84cd002830631e6
fang.liu <i@liufang.org.cn>
2025-11-27 00:02:26 +0800

    修复低级bug

--- soho-extend/soho-shop/soho-shop-biz/src/test/java/work/soho/shop/biz/controller/user/UserShopUserCouponsControllerTest.java
+++ soho-extend/soho-shop/soho-shop-biz/src/test/java/work/soho/shop/biz/controller/user/UserShopUserCouponsControllerTest.java
@@ -1,13 +1,18 @@
 package work.soho.shop.biz.controller.user;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
 import work.soho.test.TestApp;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers.springSecurity;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -18,8 +23,20 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @ActiveProfiles("dev")
 class UserShopUserCouponsControllerTest {
 
+    private MockMvc mockMvc;
+
+    @Autowired
+    private WebApplicationContext webApplicationContext;
+
+    @BeforeEach
+    public void setup() {
+        this.mockMvc = MockMvcBuilders.webAppContextSetup(this.webApplicationContext)
+                .apply(springSecurity())
+                .build();
+    }
+
     @Test
-    void getUserCoupons() {
+    void getUserCoupons() throws Exception {
         mockMvc.perform(get("/shop/user/shopUserCoupons/getUserCoupons"))
                 //.andExpect(status().is(404))
                 .andDo(print())
