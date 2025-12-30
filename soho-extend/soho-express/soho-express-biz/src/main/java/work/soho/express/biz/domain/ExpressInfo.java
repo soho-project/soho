8a7b26cca0b6771982354b4bdb32712b19ababfa
fang.liu <i@liufang.org.cn>
2025-12-30 11:30:01 +0800

    完成配置信息从数据库读取

--- soho-extend/soho-express/soho-express-biz/src/main/java/work/soho/express/biz/domain/ExpressInfo.java
+++ soho-extend/soho-express/soho-express-biz/src/main/java/work/soho/express/biz/domain/ExpressInfo.java
@@ -1,13 +1,14 @@
 package work.soho.express.biz.domain;
 
-import com.alibaba.excel.annotation.ExcelProperty;
-import com.alibaba.excel.annotation.format.DateTimeFormat;
+import io.swagger.annotations.ApiModelProperty;
 import com.baomidou.mybatisplus.annotation.*;
 import com.fasterxml.jackson.annotation.JsonFormat;
-import io.swagger.annotations.ApiModelProperty;
-import lombok.Data;
-
+import com.alibaba.excel.annotation.ExcelProperty;
+import com.alibaba.excel.annotation.format.DateTimeFormat;
 import java.io.Serializable;
+import lombok.Data;
+import java.math.BigDecimal;
+import java.time.LocalDate;
 import java.time.LocalDateTime;
 
 @Data
@@ -20,7 +21,7 @@ public class ExpressInfo implements Serializable {
     @ApiModelProperty(value = "id")
     @TableId(value = "id", type = IdType.AUTO)
     @JsonFormat(shape = JsonFormat.Shape.STRING)
-    private Long id;
+    private Integer id;
 
     /**
     * 快递名
@@ -83,5 +84,77 @@ public class ExpressInfo implements Serializable {
     @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
     @DateTimeFormat("yyyy-MM-dd HH:mm:ss")
     private LocalDateTime createdTime;
+
+    /**
+    * 发件人名称
+    */
+    @ExcelProperty("发件人名称")
+    @ApiModelProperty(value = "发件人名称")
+    @TableField(value = "sender_name")
+    private String senderName;
+
+    /**
+    * 发件人座机
+    */
+    @ExcelProperty("发件人座机")
+    @ApiModelProperty(value = "发件人座机")
+    @TableField(value = "sender_phone")
+    private String senderPhone;
+
+    /**
+    * 发件人手机
+    */
+    @ExcelProperty("发件人手机")
+    @ApiModelProperty(value = "发件人手机")
+    @TableField(value = "sender_mobile")
+    private String senderMobile;
+
+    /**
+    * 发件人省份
+    */
+    @ExcelProperty("发件人省份")
+    @ApiModelProperty(value = "发件人省份")
+    @TableField(value = "sender_province")
+    private String senderProvince;
+
+    /**
+    * 发件人城市
+    */
+    @ExcelProperty("发件人城市")
+    @ApiModelProperty(value = "发件人城市")
+    @TableField(value = "sender_city")
+    private String senderCity;
+
+    /**
+    * 发件人县/区
+    */
+    @ExcelProperty("发件人县/区")
+    @ApiModelProperty(value = "发件人县/区")
+    @TableField(value = "sender_district")
+    private String senderDistrict;
+
+    /**
+    * 发件人地址
+    */
+    @ExcelProperty("发件人地址")
+    @ApiModelProperty(value = "发件人地址")
+    @TableField(value = "sender_address")
+    private String senderAddress;
+
+    /**
+    * 面单账号
+    */
+    @ExcelProperty("面单账号")
+    @ApiModelProperty(value = "面单账号")
+    @TableField(value = "bill_account")
+    private String billAccount;
+
+    /**
+    * 面单密码
+    */
+    @ExcelProperty("面单密码")
+    @ApiModelProperty(value = "面单密码")
+    @TableField(value = "bill_account_password")
+    private String billAccountPassword;
 
 }
