3bc71e618d21c65fdeec487144929b1857d5b85c
fang.liu <i@liufang.org.cn>
2026-01-14 22:39:02 +0800

    完成开放平台鉴权改造

--- soho-extend/soho-open/soho-open-biz/src/main/java/work/soho/open/biz/aspect/OpenApiAspect.java
+++ soho-extend/soho-open/soho-open-biz/src/main/java/work/soho/open/biz/aspect/OpenApiAspect.java
@@ -16,12 +16,10 @@ import org.springframework.web.context.request.ServletRequestAttributes;
 import work.soho.common.core.result.R;
 import work.soho.common.core.util.IpUtils;
 import work.soho.open.api.annotation.OpenApi;
-import work.soho.open.biz.component.BodyCachingHttpServletRequestWrapper;
 import work.soho.open.biz.component.OpenApiLimitFacotory;
 import work.soho.open.biz.domain.OpenApiCallLog;
 import work.soho.open.biz.domain.OpenApiStatDay;
 import work.soho.open.biz.domain.OpenApp;
-import work.soho.open.biz.domain.OpenAppIpWhitelist;
 import work.soho.open.biz.enums.OpenAppEnums;
 import work.soho.open.biz.service.*;
 
@@ -29,11 +27,13 @@ import javax.servlet.http.HttpServletRequest;
 import java.lang.reflect.Method;
 import java.security.InvalidParameterException;
 import java.time.LocalDate;
-import java.util.*;
+import java.util.List;
 
-import static cn.hutool.crypto.SecureUtil.md5;
-
-
+/**
+ * 这里不做验签，
+ *
+ * 这里做限流，计数，访问权限灯验证
+ */
 @Log4j2
 @Aspect
 @Component
@@ -54,12 +54,11 @@ public class OpenApiAspect {
             ServletRequestAttributes attrs = (ServletRequestAttributes) RequestContextHolder.getRequestAttributes();
             HttpServletRequest request = attrs.getRequest();
             String appKey = request.getHeader("app-key");
-            String sign = request.getHeader("sign");
             String reqTime = request.getHeader("req-time");
             String method = request.getMethod();
             String path = request.getRequestURI();
 
-            if (appKey == null || sign == null || reqTime == null) {
+            if (appKey == null) {
                 throw new InvalidParameterException("缺少必要请求头");
             }
 
@@ -79,19 +78,9 @@ public class OpenApiAspect {
             if (openApp == null) {
                 throw new InvalidParameterException("无效 app-key");
             }
-            String appSecret = openApp.getAppSecret();
-
-            if (appSecret == null) {
-                throw new InvalidParameterException("无效 app-/");
-            }
 
             // 获取api信息
             work.soho.open.biz.domain.OpenApi api = openApiService.getByMethodAndPathAndVersion(method, path, "1.0.0");
-
-            // 检查签名
-            if(!checkKey(request, openApp, sign, appKey, reqTime, appSecret, method, path)) {
-                return R.error(2401, "鉴权失败");
-            }
 
             // 检查接口是否允许访问
             if (!canAccess(openApp, method, path)) {
@@ -165,97 +154,6 @@ public class OpenApiAspect {
         }
     }
 
-    private boolean checkKey(HttpServletRequest request, OpenApp openApp, String sign, String appKey, String reqTime,
-                             String appSecret, String method, String path) {
-        try {
-            // 2. 计算服务端签名
-            String serverSign = getSignByRequest(request, appSecret);
-            log.info("sign: " + sign);
-            log.info("serverSign: " + serverSign);
-            log.info("reqTime: " + reqTime);
-
-            // 3. 对比签名
-            if (!serverSign.equalsIgnoreCase(sign)) {
-                throw new InvalidParameterException("签名校验失败");
-            }
-
-            // 4. （可选）校验时间戳
-            if (!checkReqTime(reqTime)) {
-                throw new InvalidParameterException("请求已过期");
-            }
-
-            // 6. 验证 IP 白名单
-            String clientIp = IpUtils.getClientIp();
-            List<OpenAppIpWhitelist> openAppIpWhitelists = openAppIpWhitelistService.getByAppId(openApp.getId());
-            boolean isInWhitelist = false;
-            for (OpenAppIpWhitelist openAppIpWhitelist : openAppIpWhitelists) {
-                if (IpUtils.inSubnet(openAppIpWhitelist.getIp(), clientIp)) {
-                    isInWhitelist = true;
-                }
-            }
-            if(!isInWhitelist) {
-                return false;
-            }
-
-            return true;
-        } catch (Exception e) {
-            log.error(e);
-            return false;
-        }
-    }
-
-    private String getSignByRequest(HttpServletRequest request, String appSecret) throws Exception {
-        String method = request.getMethod();
-        String body = "";
-
-        if(method.equalsIgnoreCase("GET")) {
-            LinkedHashMap<String, String> linkedHashMap = new LinkedHashMap<>();
-            Map<String, String[]> parameterMap = request.getParameterMap();
-            for (Map.Entry<String, String[]> entry : parameterMap.entrySet()) {
-                String key = entry.getKey();
-                String[] values = entry.getValue();
-                if (values != null && values.length > 0) {
-                    // 只取第一个值
-                    linkedHashMap.put(key, values[0]);
-                } else {
-                    linkedHashMap.put(key, null);
-                }
-            }
-            body = buildQueryString(linkedHashMap);
-        } else {
-            body = ((BodyCachingHttpServletRequestWrapper)request).getBody();
-        }
-
-        log.info("sign body:" + body + "_" + appSecret);
-        String sign = md5(body + "_" + appSecret);
-        return sign;
-    }
-
-    private static String buildQueryString(Object obj) throws Exception {
-        Map<String, Object> map;
-        map = (Map<String, Object>) obj;
-
-        // 对 Map 的键进行排序
-        List<String> sortedKeys = new ArrayList<>(map.keySet());
-        Collections.sort(sortedKeys); // 或使用 sortedKeys.sort(Comparator.naturalOrder())
-
-        StringBuilder sb = new StringBuilder();
-        for (String key : sortedKeys) {
-            Object value = map.get(key);
-            if (value != null) {
-                sb.append(key)
-                        .append("=")
-                        .append(java.net.URLEncoder.encode(value.toString(), "UTF-8"))
-                        .append("&");
-            }
-        }
-
-        if (sb.length() > 0) {
-            sb.setLength(sb.length() - 1); // 去掉最后一个 &
-        }
-        return sb.toString();
-    }
-
     private boolean canAccess(OpenApp openApp, String method, String path) {
         if (openApp == null) {
             return false;
@@ -289,20 +187,6 @@ public class OpenApiAspect {
         return true;
     }
 
-    private boolean checkReqTime(String reqTime) {
-        // 获取当前时间戳
-        long currentTime = System.currentTimeMillis();
-
-        // 计算时间差（毫秒）
-        long timeDiff = currentTime - Long.parseLong(reqTime) * 1000;
-
-        // 设置允许的时差范围（毫秒）
-        long allowedTimeDiff = 5 * 60 * 1000; // 5 分钟
-
-        // 检查时间差是否在允许范围内
-        return timeDiff <= allowedTimeDiff;
-    }
-
     private OpenApi getOpenApi(ProceedingJoinPoint invocation) {
         String methodName=invocation.getSignature().getName();
         Class<?> classTarget=invocation.getTarget().getClass();
@@ -314,6 +198,4 @@ public class OpenApiAspect {
             return null;
         }
     }
-
-
 }
