070e0849050061733523b5cdeb1f02d9c00550a4
fang <i@liufang.org.cn>
2026-02-09 00:00:21 +0800

    整理枚举

--- soho-extend/soho-ai/soho-ai-biz/src/main/java/work/soho/ai/biz/enums/AiAppEnums.java
+++ soho-extend/soho-ai/soho-ai-biz/src/main/java/work/soho/ai/biz/enums/AiAppEnums.java
@@ -4,4 +4,13 @@ import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 
 public class AiAppEnums {
+
+    @RequiredArgsConstructor
+    @Getter
+    public enum Status {
+        DISABLED(0,"禁用"),
+        ENABLE(1,"启用");
+        private final int id;
+        private final String name;
+    }
 }
